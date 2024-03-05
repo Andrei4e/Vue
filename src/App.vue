2de02ded2f9a4f72b2074910279f6345ae51f4a2
Andrei <Andrei@mail.ru>
2024-03-05 12:55:25 +0300

    готовое дз

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <!-- <HomePage /> -->
+    <!-- <BlogPage /> -->
+    <BlogDetailsPage />
+  </div>
+</template>
+
+<script>
+// import HomePage from "./components/HomePage.vue";
+//import BlogPage from "./components/BlogPage.vue";
+import BlogDetailsPage from "./components/BlogDetailsPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    // HomePage,
+    //BlogPage,
+    BlogDetailsPage
+  },
+};
+</script>
+
+<style>
+/* #app {
+  font-family: 'Times New Roman', Times, serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
