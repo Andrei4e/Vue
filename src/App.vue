f3771ab5378a4e60d512fbc48f6c2d688e505e44
Andrei <Andrei@mail.ru>
2024-03-12 15:58:51 +0300

    готовое дз (урок 10)

--- src/App.vue
+++ src/App.vue
@@ -2,32 +2,28 @@
   <div id="app">
     <!-- <HomePage /> -->
     <!-- <BlogPage /> -->
-    <BlogDetailsPage />
+    <!-- <BlogDetailsPage /> -->
+    <ProductDetails />
   </div>
 </template>
 
 <script>
 // import HomePage from "./components/HomePage.vue";
 //import BlogPage from "./components/BlogPage.vue";
-import BlogDetailsPage from "./components/BlogDetailsPage.vue";
+//import BlogDetailsPage from "./components/BlogDetailsPage.vue";
+import ProductDetails from "./components/ProductDetails.vue";
 
 export default {
   name: "App",
   components: {
     // HomePage,
     //BlogPage,
-    BlogDetailsPage
+    //BlogDetailsPage,
+    ProductDetails
   },
 };
 </script>
 
 <style>
-/* #app {
-  font-family: 'Times New Roman', Times, serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+
 </style>
